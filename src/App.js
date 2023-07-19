e20c833e36a7e4cffc38ec1762064b58727dc99d
lavaxcii <lavaxcii@gmail.com>
2023-07-19 11:28:22 +0200

    Refactor everything; Add some styling all around.

--- src/App.js
+++ src/App.js
@@ -1,90 +1,68 @@
 import React, { useState } from 'react';
 import './index.css';
+import cardsData from './json/cards';
 
 function App() {
-  const [cards, setCards] = useState(
-    [{
-      key: 0,
-      clicked: false,
-      id: 0,
-      gallery: require('../src/img/bulma.jpg')
-    },
-    {
-      key: 1,
-      clicked: false,
-      id: 1,
-      gallery: require('../src/img/goku1.jpg')
-    },
-    {
-      key: 2,
-      clicked: false,
-      id: 2,
-      gallery: require('../src/img/goku2.jpg')
-    },
-    {
-      key: 3,
-      clicked: false,
-      id: 3,
-      gallery: require('../src/img/goku3.jpg')
-    },
-    {
-      key: 4,
-      clicked: false,
-      id: 4,
-      gallery: require('../src/img/goku4.jpg')
-    },
-    {
-      key: 5,
-      clicked: false,
-      id: 5,
-      gallery: require('../src/img/krilin.jpg')
-    },
-    {
-      key: 6,
-      clicked: false,
-      id: 6,
-      gallery: require('../src/img/krme.jpg')
-    },
-    {
-      key: 7,
-      clicked: false,
-      id: 7,
-      gallery: require('../src/img/malibijeli.jpg')
-    },
-    {
-      key: 8,
-      clicked: false,
-      id: 8,
-      gallery: require('../src/img/pikolo.jpg')
-    }
-    ]
-  );
-  // const [changeClickedStatus, setChangeClickedStatus] = useState(null)
+  const [cards, setCards] = useState([...cardsData]);
+  const [initialCardsOrder, setInitialCardsOrder] = useState([...cardsData]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  // const [clickedCardId, setClickedCardId] = useState(null);
 
-  const updateSetCards = (cardIdValue) => {
-    // trebaš spread operator iskoristiti da ostatak arraya preneseš!
-    setCards(cards[cardIdValue].clicked = true);
+  const shuffleCards = () => {
+    let mutateCards = [...cards]
+    // shuffle cards
+    const rng = () => {
+      return Math.floor(Math.random() * (8 - 0) + 0);
+    };
+    let newCardsOrder = [];
+    for (let i = 0; i <= 8; i++) {
+      let randomIndex = rng();
+      do {
+        randomIndex = rng();
+      } while (randomIndex >= mutateCards.length)
+      let itemForShuffle = mutateCards.splice(randomIndex, 1);
+      newCardsOrder.push(itemForShuffle[0]);
+    };
+    setCards([...newCardsOrder]);
   };
-  console.table(cards);
 
-  // let cardsToMap = cards;
-
-  // stateovi za karte
-  // funkcije/metode za praćenje trenutnih bodova
-  // i za najvećeg broja bodova, te za mješanje karti
-  // arraya objekata (u stateu?) podataka o kartama
+  const updateSetCards = (cardIdValue, cardObject) => {
+    let mutateCards = [...cards];
+    if (cardObject.clicked === true) {
+      mutateCards = [...initialCardsOrder];
+      for (let i = 0; i <= 8; i ++) {
+        mutateCards[i].clicked = false;
+      };
+      setCards([...mutateCards]);
+      if (currentScore >= bestScore) {
+        setBestScore(currentScore);
+        setCurrentScore(0);
+      } else {
+        setCurrentScore(0);
+      };
+    } else {
+      //* this should not work
+      //* but it works
+      //* anyway, this doesn't seem right, revisit and refactor
+      cardObject.clicked = true;
+      setCurrentScore(currentScore + 1);
+      setCards([...mutateCards]);
+      shuffleCards();
+    };
+  };
 
   return (
-    <div className="min-h-screen flex flex-col items-center bg-gradient-to-r from-blue-200 to-blue-300">
-      <div className="text-2xl flex flex-row items-start pb-10 pr-10 pt-10 bg-gradient-to-r from-blue-400 to-blue-200">
-        <p>current score: </p>
-        <p>best score: </p>
+    <div className="min-h-screen flex flex-col items-center bg-gradient-to-r from-blue-400 to-blue-800">
+      <div className="text-2xl flex flex-row items-start pb-10 pr-10 pt-10 space-x-4">
+        <p>current score: {currentScore}</p>
+        <p>best score:{bestScore}</p>
       </div>
       <div className="m-2 pb-20 grid gap-4 grid-cols-3 grid-rows-3 items-center">
         {cards.map(card => {
           return (
-            <div className='text-xl text-white flex flex-col content-start items-center justify-start bg-gradient-to-r from-slate-300 to-slate-800 border-slate-200 border-solid border-8  hover:text-green-400 hover:from-slate-500 hover:to-black hover:border-green-500 hover:border-solid hover:border-8' key={card.id} onClick={() => updateSetCards(card.id)}>
-              <p>Card #{card.id + 1}</p>
+            <div className='text-xl text-white flex flex-col content-start items-center justify-start bg-gradient-to-r from-slate-300 to-slate-800 border-slate-200 border-solid border-8  hover:text-green-400 hover:from-slate-500 hover:to-black hover:border-green-500 hover:border-solid hover:border-8' key={card.id} onClick={() => updateSetCards(card.id, card)}>
+              <p>Card #{card.id}</p>
               <img src={card.gallery} alt="" className='h-[162px] w-[243px]' />
             </div>
           )
---
+++ src/json/cards.js
@@ -0,0 +1,57 @@
+const cardsData = [{
+  key: 0,
+  clicked: false,
+  id: 0,
+  gallery: require('../img/bulma.jpg')
+},
+{
+  key: 1,
+  clicked: false,
+  id: 1,
+  gallery: require('../img/goku1.jpg')
+},
+{
+  key: 2,
+  clicked: false,
+  id: 2,
+  gallery: require('../img/goku2.jpg')
+},
+{
+  key: 3,
+  clicked: false,
+  id: 3,
+  gallery: require('../img/goku3.jpg')
+},
+{
+  key: 4,
+  clicked: false,
+  id: 4,
+  gallery: require('../img/goku4.jpg')
+},
+{
+  key: 5,
+  clicked: false,
+  id: 5,
+  gallery: require('../img/krilin.jpg')
+},
+{
+  key: 6,
+  clicked: false,
+  id: 6,
+  gallery: require('../img/krme.jpg')
+},
+{
+  key: 7,
+  clicked: false,
+  id: 7,
+  gallery: require('../img/malibijeli.jpg')
+},
+{
+  key: 8,
+  clicked: false,
+  id: 8,
+  gallery: require('../img/pikolo.jpg')
+}
+];
+
+export default cardsData;
